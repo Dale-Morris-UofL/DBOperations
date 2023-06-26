c6d42184113c830f6ab1ccfb4cac83487acf9fe8
Dale-Morris-UofL <137803582+Dale-Morris-UofL@users.noreply.github.com>
2023-06-26 10:40:11 -0400

    Add files via upload

---
+++ src/select.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_DATA 100
+
+int linearSearch(char *str, char *strArray[]);
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("An unexpected number of arguments was given.\n");
+
+        return 1;
+    }
+
+    FILE *inFile;
+    int index = linearSearch("-i", argv);
+    if (index >= 0) {
+        inFile = fopen(argv[index+1], "r");
+    } else {
+        inFile = fopen(stdin, "r");
+    }
+
+    if (inFile == NULL) {
+        printf("This file cannot be opened.\n");
+        
+        return 1;
+    }
+
+    char str[MAX_DATA] = "";
+    while (fgets(str, MAX_DATA - 1, inFile) != NULL) {
+        printf("%s", str);
+    }
+
+    fclose(inFile);
+
+    return 0;
+}
+
+int linearSearch(char *str, char *strArray[]) {
+    int i = 0;
+    while (strArray[i] != NULL) {
+        if (!strcomp(str, strArray[i])) {
+            return i;
+        }
+
+        i++;
+    }
+
+    return -1;
+}
