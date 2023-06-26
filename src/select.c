7a4e8ab572544dacb02f69a9653e250feff912bd
Dale Morris <thatdaleguy03@owl.lan>
2023-06-26 17:07:41 -0400

    Added output file functionality and header detection

--- src/select.c
+++ src/select.c
@@ -6,33 +6,64 @@
 int linearSearch(char *str, char *strArray[]);
 
 int main(int argc, char *argv[]) {
-    if (argc > 3) {
+    // Checks for a sensible number of arguments
+    if (argc > 5) {
         printf("An unexpected number of arguments was given.\n");
 
         return 1;
     }
 
+    // Initializes variables
+    char str[MAX_DATA] = "";
+    int index = 0;
+    int hasHeader = (linearSearch("-h", argv) >= 0) ? 1 : 0;
+
+    // Sets input file
     FILE *inFile;
-    int index = linearSearch("-i", argv);
+    index = linearSearch("-i", argv);
     if (index >= 0) {
+        if ((argc <= index+1) | (argv[index+1][0] == '-')) {
+            printf("\"-i\" should be followed by a file name.\n");
+
+            return 1;
+        }
+
         inFile = fopen(argv[index+1], "r");
     } else {
         inFile = stdin;
     }
 
     if (inFile == NULL) {
-        printf("This file cannot be opened.\n");
+        printf("File cannot be opened.\n");
         
         return 1;
     }
 
-    char str[MAX_DATA] = "";
-    while (fgets(str, MAX_DATA - 1, inFile) != NULL) {
-        printf("%s", str);
+    // Sets output file
+    FILE *outFile;
+    index = linearSearch("-o", argv);
+    if (index >= 0) {
+        if ((argc <= index+1) | (argv[index+1][0] == '-')) {
+            printf("\"-o\" should be followed by a file name.\n");
+
+            return 1;
+        }
+
+        outFile = fopen(argv[index+1], "w");
+    } else {
+        outFile = stdout;
     }
 
-    fclose(inFile);
+    // Gets and prints data
+    while ((fgets(str, MAX_DATA - 1, inFile) != NULL) && (strcmp(str, "\n"))) {
+        fprintf(outFile, "%s", str);
+    }
 
+    // Frees resources
+    fclose(inFile);
+    fclose(outFile);
+
+    // Returns
     return 0;
 }
 
