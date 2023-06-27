0e106d591351356a195030461f40fb420f61c6c8
Dale Morris <thatdaleguy03@owl.lan>
2023-06-27 16:39:23 -0400

    Minor changes

--- src/select.c
+++ src/select.c
@@ -7,7 +7,7 @@
  * NOTES
  *
  * Program will work even if extra information is added in the command line with no tag.
- * E.g. ./select -c salary>2000 -h this-should-not-be-here -i ../data/employees.csv -o ../data/output.csv
+ * E.g. select -c "salary > 2000" -h this-should-not-be-here -i ../data/employees.csv -o ../data/output.csv
  * This should probably be fixed.
  */
 
@@ -16,28 +16,30 @@ int linearSearch(char *str, char *strArray[]);
 int main(int argc, char *argv[]) {
     // Checks for a sensible number of arguments
     if (argc > 8) {
-        printf("An unexpected number of arguments was given.\n");
+        fprintf(stderr, "An unexpected number of arguments was given.\n");
 
         return 1;
     }
 
     // Initializes variables
+    int i = 0;
     char str[MAX_DATA] = "";
     int index = 0;
     int hasHeader = (linearSearch("-h", argv) >= 0) ? 1 : 0;
 
+    // Gets condition
     char *condition = "";
     index = linearSearch("-c", argv);
     if (index >= 0) {
         if ((argc <= index+1) || (argv[index+1][0] == '-')) {
-            printf("\"-c\" should be followed by a condition.\n");
+            fprintf(stderr, "\"-c\" should be followed by a condition.\n");
 
             return 1;
         }
 
         condition = argv[index+1];
     } else {
-        printf("The \"-c\" tag is required.\n");
+        fprintf("The \"-c\" tag is required.\n");
 
         return 1;
     }
@@ -47,7 +49,7 @@ int main(int argc, char *argv[]) {
     index = linearSearch("-i", argv);
     if (index >= 0) {
         if ((argc <= index+1) || (argv[index+1][0] == '-')) {
-            printf("\"-i\" should be followed by a file name.\n");
+            fprintf(stderr, "\"-i\" should be followed by a file name.\n");
 
             return 1;
         }
@@ -58,7 +60,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (inFile == NULL) {
-        printf("File cannot be opened.\n");
+        fprintf(stderr, "File cannot be opened.\n");
         fclose(inFile);
 
         return 1;
@@ -69,7 +71,7 @@ int main(int argc, char *argv[]) {
     index = linearSearch("-o", argv);
     if (index >= 0) {
         if ((argc <= index+1) || (argv[index+1][0] == '-')) {
-            printf("\"-o\" should be followed by a file name.\n");
+            fprintf(stderr, "\"-o\" should be followed by a file name.\n");
             fclose(inFile);
 
             return 1;
@@ -80,8 +82,9 @@ int main(int argc, char *argv[]) {
         outFile = stdout;
     }
 
+    // Temporary code for debugging purposes
     printf("hasHeader: %d\n", hasHeader);
-    printf("Condition: %s\n", condition);
+    printf("condition: %s\n", condition);
 
     // Gets and prints data
     while ((fgets(str, MAX_DATA - 1, inFile) != NULL) && (strcmp(str, "\n"))) {
