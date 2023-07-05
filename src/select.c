8d9bb5acad12c451f2937841f6276dc4ef22a498
Dale Morris <thatdaleguy03@owl.lan>
2023-07-05 16:13:46 -0400

    Broken arg checking

--- src/select.c
+++ src/select.c
@@ -5,19 +5,15 @@
 
 #define MAX_DATA 100
 
-/*
- * NOTES
- *
- * Program will work even if extra information is added in the command line with no tag.
- * E.g. select -c "salary > 2000" "this should not be here" -i ../data/employees.csv -o ../data/output.csv
- * This should probably be fixed.
- */
-
 typedef struct Condition {
     char *operator;
     char *operand1;
     char *operand2;
 } Condition;
+
+int badArgs(int argc, char *argv[]);
+
+int *findTags(char *argv[]);
 
 int linearSearch(char *str, char *strArray[]);
 
@@ -28,49 +24,46 @@ Condition *copyCondition(Condition *condition);
 void Condition_destroy(Condition *condition);
 
 int main(int argc, char *argv[]) {
-    // Checks for a sensible number of arguments
-    if (argc > 8) {
-        fprintf(stderr, "An unexpected number of arguments was given.\n");
-
-        return 1;
+    // Checks args
+    int argCheck = badArgs(argc, argv);
+    switch (argCheck) {
+        case 1:
+            fprintf(stderr, "There was an unexpected number of args.\n");
+            return 1;
+        case 2:
+            fprintf(stderr, "The \"-c\" tag must be included.\n");
+            return 1;
+        case 3:
+        case 4:
+        case 5:
+            fprintf(stderr, "Incorrect form. Try \"select -c <condition> [-h] [-i input-file] [-o output-file]\".\n");
+            return 1;
     }
+    int *tagIndices = findTags(argv);
 
     // Initializes variables
-    int index = 0;
-    int hasHeader = (linearSearch("-h", argv) >= 0) ? 1 : 0;
+    int hasHeader = (tagIndices[1] >= 0) ? 1 : 0;
     char line[MAX_DATA] = "";
 
+    int temp = 0; // Debug
+    for (temp = 0; temp < 4; temp++) { // Debug
+        printf("Tag index %d: %d\n", temp, tagIndices[temp]);
+    }
     // Gets condition
     Condition *mainCondition;
-    char *conditionString = "";
-    index = linearSearch("-c", argv);
-    if (index >= 0) {
-        if ((argc <= index+1) || (argv[index+1][0] == '-')) {
-            fprintf(stderr, "\"-c\" should be followed by a condition.\n");
-
-            return 1;
-        }
-
-        conditionString = argv[index+1];
-    } else {
-        fprintf(stderr, "The \"-c\" tag is required.\n");
-
-        return 1;
-    }
-
+    char *conditionString = argv[tagIndices[0]+1];
     mainCondition = interpretCondition(conditionString, hasHeader);
 
+    for (temp = 0; temp < 4; temp++) { // Debug
+        printf("Tag index %d: %d\n", temp, tagIndices[temp]);
+    }
+    printf("After condition, before input\n"); // Debug
+    printf("Index of -i: %d\n", tagIndices[2]); // Debug
+    printf("Input file: %s\n", argv[4 + 1]); // Debug
     // Sets input file
     FILE *inFile;
-    index = linearSearch("-i", argv);
-    if (index >= 0) {
-        if ((argc <= index+1) || (argv[index+1][0] == '-')) {
-            fprintf(stderr, "\"-i\" should be followed by a file name.\n");
-
-            return 1;
-        }
-
-        inFile = fopen(argv[index+1], "r");
+    if (tagIndices[2] >= 0) {
+        inFile = fopen(argv[tagIndices[2]+1], "r");
     } else {
         inFile = stdin;
     }
@@ -82,21 +75,17 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    printf("After input, before output\n"); // Debug
+
     // Sets output file
     FILE *outFile;
-    index = linearSearch("-o", argv);
-    if (index >= 0) {
-        if ((argc <= index+1) || (argv[index+1][0] == '-')) {
-            fprintf(stderr, "\"-o\" should be followed by a file name.\n");
-            fclose(inFile);
-
-            return 1;
-        }
-
-        outFile = fopen(argv[index+1], "w");
+    if (tagIndices[3] >= 0) {
+        outFile = fopen(argv[tagIndices[3]+1], "w");
     } else {
         outFile = stdout;
     }
+
+    printf("After output, end of test 1\n"); // Debug
 
     // Temporary code for debugging purposes
     printf("hasHeader: %d\n", hasHeader);
@@ -170,6 +159,108 @@ int main(int argc, char *argv[]) {
 
     // Returns
     return 0;
+}
+
+/*
+ * Returns:
+ * 0 if args are good
+ * 1 if there are too many args
+ * 2 if there is no -c tag
+ * 3 if there are duplicate tags
+ * 4 if there is a -c, -i, or -o tag with no corresponding non-tag arg
+ * 5 if there is a non-tag arg with no corresponding tag
+ */
+int badArgs(int argc, char *argv[]) {
+    int i = 0;
+
+    if (argc > 8) {
+        return 1;
+    }
+    
+    int hasCondition = 0;
+    int hasHeader = 0;
+    int hasInput = 0;
+    int hasOutput = 0;
+    char *previousArg = strdup(argv[0]);
+    for (i = 1; i < argc; i++) {
+        if (!strcmp("-c", argv[i])) {
+            if (hasCondition) {
+                return 3;
+            }
+
+            if (!strcmp("-i", previousArg) || !strcmp("-o", previousArg)) {
+                return 4;
+            }
+
+            hasCondition = 1;
+            previousArg = strdup(argv[i]);
+            continue;
+        }
+
+        if (!strcmp("-h", argv[i])) {
+            if (hasHeader) {
+                return 3;
+            }
+
+            hasHeader = 1;
+            previousArg = strdup(argv[i]);
+            continue;
+        }
+
+        if (!strcmp("-i", argv[i])) {
+            if (hasInput) {
+                return 3;
+            }
+
+            if (!strcmp("-c", previousArg) || !strcmp("-o", previousArg)) {
+                return 4;
+            }
+
+            hasInput = 1;
+            previousArg = strdup(argv[i]);
+            continue;
+        }
+
+        if (!strcmp("-o", argv[i])) {
+            if (hasOutput) {
+                return 3;
+            }
+
+            if (!strcmp("-c", previousArg) || !strcmp("-i", previousArg)) {
+                return 4;
+            }
+
+            hasOutput = 1;
+            previousArg = strdup(argv[i]);
+            continue;
+        }
+
+        if (strcmp("-c", previousArg) && strcmp("-i", previousArg) && strcmp("-o", previousArg)) {
+            return 5;
+        }
+
+        previousArg = strdup(argv[i]);
+    }
+    if (!hasCondition) {
+        return 2;
+    }
+    if (!strcmp("-c", previousArg) || !strcmp("-i", previousArg) || !strcmp("-o", previousArg)) {
+        return 4;
+    }
+
+    return 0;
+}
+
+int *findTags(char *argv[]) {
+    int tagIndices[4];
+
+    tagIndices[0] = linearSearch("-c", argv);
+    tagIndices[1] = linearSearch("-h", argv);
+    tagIndices[2] = linearSearch("-i", argv);
+    tagIndices[3] = linearSearch("-o", argv);
+
+    int *tagPointer = tagIndices;
+    return tagPointer;
 }
 
 // Basic linear search for an array of strings
